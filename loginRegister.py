9db17cb1c5d3b160971b4ea2b50a9478f9348aae
CirrusPreeminent <153276550+CirrusPreeminent@users.noreply.github.com>
2025-04-12 04:17:58 +0700

    add comment to everything

--- loginRegister.py
+++ loginRegister.py
@@ -4,59 +4,61 @@ from crypto_utils import aes_decrypt, rsa_decrypt
 with open("config.json", "r") as file:
     config = json.load(file)
 
+#### ใช้ เซฟข้อมูล user ลงใน database ตอน register
 def save_user(username, password):
     # คิดว่าจะต้อง hash password ตรงนี้ เเล้วค่อยเก็บลง txt file
-    # อาจะลองgenerate key ที่จะใช้ encrypy && decrypt เเล้วเก็บไว้ใน config.json ก็ได้ ที่มันเป็น ciphertext ไม่รู้เวิคไหม
     with open(config["USER_DB_FILE"], "a") as file:
         file.write(f"{username},{password}\n")
 
-# client_socket เก็บข้อมูลของ client src-des ip, protocol etc.
-# users เก็บข้อมูลที่อยู่ใน user_db.txt
+
+#### ใช้สำหรับ register
 def register(client_socket, users, private_key):
-    client_socket.send(b"Enter new username: ")
-    username = client_socket.recv(1024)
-    username = decryptMessage(username, private_key)
+    client_socket.send(b"Enter new username: ") # ส่ง Prompt ให้ กรอก username ไปยัง client_socket
+    username = client_socket.recv(1024) # รอรับข้อความจาก client_socket
+    username = decryptMessage(username, private_key) #ถอดรหัสข้อความ AES ที่ถูก encrypt ซ้อนมาด้วย publick key ของ server
 
     client_socket.send(b"Enter new password: ")
     password = client_socket.recv(1024)
     password = decryptMessage(password, private_key)
 
-    if username in users:
+    if username in users: #### เช็คว่า username ซ้ำมั้ยจาก global variable users{} ที่กำหนดไว้ใน tcp-threaded-server ### ​!!! ควรแก้ !!! 
         client_socket.send(b"Username already exists. Try again.\n")
         return None
     else:
         users[username] = password
-        save_user(username, password)
+        save_user(username, password) ### เรียก function "save_user" เพื่อเซฟลง database
         client_socket.send(b"Registration successful. You can now login.\n")
         return None
     
+### ใช้สหรับ login 
 def login(client_socket, users, private_key):
-    client_socket.send(b"Enter username: ")
-    username = client_socket.recv(1024)
-    username = decryptMessage(username, private_key)
+    client_socket.send(b"Enter username: ") # ส่ง Prompt ให้ กรอก username ไปยัง client_socket
+    username = client_socket.recv(1024) # รอรับข้อความจาก client_socket
+    username = decryptMessage(username, private_key) #ถอดรหัสข้อความ AES ที่ถูก encrypt ซ้อนมาด้วย public key ของ server
     
     client_socket.send(b"Enter password: ")
     password = client_socket.recv(1024)
     password = decryptMessage(password, private_key)
 
-    # คิดว่าตรงนี้จะต้อง เอารหัสที่ user input มา compare กับ hashed password ใน txt
+    # คิดว่าตรงนี้จะต้องแก้ ให้เอารหัสที่ user input มา compare กับ hashed password ใน txt ไม่ใช่จาก global variable users{}
     if username in users and users[username] == password:
         client_socket.send(b"Login successful. Welcome!\n")
         return username
     else:
         client_socket.send(b"Invalid username or password. Try again.\n")
         return None
+    
 
+### ใช้ถอดรหัสข้อความ จาก client ที่ส่งมาเป็น -> SERVER_Public_Key(Client_AES_Key(ข้อความจริงๆ))
 def decryptMessage(msg, private_key):
-    #Decrypt Message
+    # Decrypt Message
     if msg.startswith(b'ENC:'):
         try:            
             payload = msg[len(b'ENC:'):]
-            encrypted_key, encrypted_msg = payload.split(b'||')
+            encrypted_key, encrypted_msg = payload.split(b'||')  ### แยกระหว่าง encrypted AES key กับ encrypted AES message 
             
-            aes_key = rsa_decrypt(private_key, encrypted_key)  # use own private key
-            print(aes_key)
-            plain_msg = aes_decrypt(aes_key, encrypted_msg)
+            aes_key = rsa_decrypt(private_key, encrypted_key) # ถอดรหัส encrypted AES key ด้วย private key ของ server (เพราะ client ส่งข้อความาเป็น AES key ที่ encrypted ซ้อนด้วย public key ของ server)
+            plain_msg = aes_decrypt(aes_key, encrypted_msg) # ถอดรหัส encrypted AES message ด้วย AES key ที่ถอดรหัสออกมาได้
 
             return plain_msg
         except Exception as e:
--- tcp-threaded-server.py
+++ tcp-threaded-server.py
@@ -24,16 +24,128 @@ SERV_IP_ADDR = config["SERVER_IP"]
 SERV_PORT = config["SERVER_PORT"]
 MAX_CLIENTS = 2
 
-clients = {}
-client_public_keys = {}
+clients = {} # เก็บ socket address ของ client
+client_public_keys = {} # เก็บ public key ของ client ไว้แลกเปลี่ยนตอน 2 client connect ครบ
+
+
 # keep every client info in dictionary
 # {<socket info>:"username"}
 clients_lock = Lock()
 
-users = {}
+users = {} # เก็บ user_db ที่ read มา ตั้งแต่แรก ## ควรแก้ (อ่านต่อใน function "main")
 # keep username && password
 # {'Teeboy': 'pass', 'moji': '123'}
 
+
+######### เวลาอ่านโค้ด python อ่านจาก ล่าง -> บน จ้า จะเข้าใจง่ายกว่า ########
+######### เวลาอ่านโค้ด python อ่านจาก ล่าง -> บน จ้า จะเข้าใจง่ายกว่า ########
+######### เวลาอ่านโค้ด python อ่านจาก ล่าง -> บน จ้า จะเข้าใจง่ายกว่า ########
+######### เวลาอ่านโค้ด python อ่านจาก ล่าง -> บน จ้า จะเข้าใจง่ายกว่า ########
+######### เวลาอ่านโค้ด python อ่านจาก ล่าง -> บน จ้า จะเข้าใจง่ายกว่า ########
+
+
+
+#### ใช้ broadcast ข้อความ ไปยัง client ทั้งหมดที่ connect อยู่ ยกเว้นตัวเอง (sender_socket)
+#### ในกรณีของเรา ที่มี connection ได้เพียง 2 client ไม่จำเป็น!!!!! #### ควรแก้ให้ส่งไปอีก client ตรงๆ และลบ function นี้ออก
+def broadcast(message, sender_socket):
+    with clients_lock:
+        for client in clients: # ลูป clients{}
+            # เช็คว่าไม่ใช่ sender
+            if client != sender_socket:
+                try:
+                    # ส่งข้อตวามไปให้อีก client
+                    client.send(message) 
+                except:
+                    pass
+
+### ใช้แค่ตอน client login/register
+def authenticate(client_socket):
+    choice = client_socket.recv(2048) ### รับ choice ที่ user เลือก ( ควรจะได้รับมาเป็น AES message ที่ encrypt ซ้อนด้วย public key ของ server อีกที)
+
+    #### === Section การถอดรหัสข้อความ #### 
+    if choice.startswith(b'ENC:'):
+        try:            
+            payload = choice[len(b'ENC:'):]
+            encrypted_key, encrypted_msg = payload.split(b'||') ### แยกระหว่าง encrypted AES key กับ encrypted AES message 
+
+            aes_key = rsa_decrypt(private_key, encrypted_key) #ถอดรหัส encrypted AES key ด้วย private key ของ server (เพราะ client ส่งข้อความาเป็น AES key ที่ encrypted ซ้อนด้วย public key ของ server)
+            plain_msg = aes_decrypt(aes_key, encrypted_msg) #ถอดรหัส encrypted AES message ด้วย AES key ที่ถอดรหัสออกมาได้
+
+        except Exception as e:
+            print(f'[ERROR] Failed to decrypt secure message: {e}')
+
+    # เช็คดูว่า plain text เลือกช้อย 1 หรือ 2
+    ## หลังจากนี้จะเป็นการเรียก function จากไฟล์ "loginRegister.py" -> ให้ไปอ่านต่อในไฟล์นั้น
+    if plain_msg == "1":  # Register
+        register(client_socket, users, private_key) 
+    elif plain_msg == "2":  # Login
+        return login(client_socket, users, private_key)
+    else:
+        client_socket.send(b"Invalid choice: \n" + plain_msg)
+        return None
+
+
+
+#### จัดการ client แต่ละตัว ในนี้ ####
+def handle_client(client_socket, addr):
+    global clients
+
+    username = None ### เริ่มแรกยังไม่ login ให้ username = None
+
+    while username is None:  ### ลูปรอใน function "authenticate()"
+        username = authenticate(client_socket)
+
+    with clients_lock:
+        clients[client_socket] = username
+
+    ### ==== เมืื่อ user login เสร็จแล้ว ====
+    print(f"{username} from {addr} joined the chat.") 
+    ### เพิ่มเก็บ log ตรงนี้ จะได้ว่า client connect/disconnect ตอนไหนบ้าง ทำให้รู้ traffic
+
+    ### ================================
+
+    ### หลังจาก login เสร็จแล้ว จะวนลูปอยู่ในนี้ตลอด
+    while True:
+        try:
+            msg = client_socket.recv(2048) ### recv ย่อ มาจาก recieve ### กำหนดให้ msg = ข้อความที่ได้รับมาจาก socket ที่กำหนด (ตัวแปร client_socket)
+            if not msg:
+                break
+            # ถ้าข้อความเริ่มต้นด้วย "PUBKEY" แสดงว่า client พยายามส่ง Public key มาให้ server
+            if msg.startswith(b'PUBKEY:'):
+                key_data = msg[len(b'PUBKEY:'):]
+                client_public_keys[client_socket] = key_data
+
+                #### ================= ตรงนี้ miracle มาก ไม่เข้าใจเลย ใครเข้าใจฝาก comment ต่อที ========================================================== ####
+                #### *** หลักการคือ พอมันตรวจเช็คเจอ client connect เข้ามา 2 คน > มันจะส่ง public ของกันและกันให้กัน (แลกเปลี่ยน public key ระหว่าง client1 && client2) 
+                with clients_lock:
+                    for other_client, pubkey in client_public_keys.items():
+                        if other_client != client_socket: 
+                            try:
+                                client_socket.send(b'PUBKEY:' + pubkey)
+                            except:
+                                pass
+
+                broadcast(b'PUBKEY:' + key_data, client_socket)
+                continue
+                ### =============================================================================================================================== ####
+            
+            # ข้อความอื่นๆ จะถูก ส่งไป function "broadcast()" เพื่อส่งข้อความที่ได้รับไปยัง client ทั้งหมดใน server
+            broadcast(msg, client_socket)
+
+
+        except: 
+            break
+
+    ##### ลบ user ออก เมื่อ client disconnected ##### !!! สำคัญ !!! 
+    with clients_lock:
+        del clients[client_socket]
+        if client_socket in client_public_keys:
+            del client_public_keys[client_socket]
+        client_socket.close()
+        print(f"{username} disconnected.")
+
+
+### อ่านไฟล์ "user_db" แล้วเซฟเก็บไว้ใน global variable 
 def load_users():
     global users
     if os.path.exists(config["USER_DB_FILE"]):
@@ -42,93 +154,10 @@ def load_users():
                 username, password = line.strip().split(",")
                 users[username] = password
 
-def authenticate(client_socket):
-    choice = client_socket.recv(2048)
-
-    #Decrypt Message
-    if choice.startswith(b'ENC:'):
-        try:            
-            payload = choice[len(b'ENC:'):]
-            encrypted_key, encrypted_msg = payload.split(b'||')
-
-            aes_key = rsa_decrypt(private_key, encrypted_key)  # use own private key
-            plain_msg = aes_decrypt(aes_key, encrypted_msg)
-
-            # print(f'{plain_msg}\n> ', end='', flush=True) 
-        except Exception as e:
-            print(f'[ERROR] Failed to decrypt secure message: {e}')
-
-
-    if plain_msg == "1":  # Register
-        register(client_socket, users, private_key)
-    elif plain_msg == "2":  # Login
-        return login(client_socket, users, private_key)
-    else:
-        client_socket.send(b"Invalid choice: \n" + plain_msg)
-        return None
-
-def broadcast(message, sender_socket):
-    with clients_lock:
-        for client in clients:
-            # Skip sending the message back to the sender
-            if client != sender_socket:
-                try:
-                    client.send(message)  # Send the message to the client
-                except:
-                    pass
-
-def handle_client(client_socket, addr):
-    global clients
-
-    username = None
-    while username is None:
-        username = authenticate(client_socket)
-
-    with clients_lock:
-        clients[client_socket] = username
-
-    print(f"{username} from {addr} joined the chat.")
-
-    while True:
-        try:
-            msg = client_socket.recv(2048)
-            if not msg:
-                break
-            # Handle public key message
-            if msg.startswith(b'PUBKEY:'):
-                key_data = msg[len(b'PUBKEY:'):]
-                client_public_keys[client_socket] = key_data
-
-                # Send existing clients' keys to the new one
-                with clients_lock:
-                    for other_client, pubkey in client_public_keys.items():
-                        if other_client != client_socket:
-                            try:
-                                client_socket.send(b'PUBKEY:' + pubkey)
-                            except:
-                                pass
-
-                # Broadcast this new client's key to others
-                broadcast(b'PUBKEY:' + key_data, client_socket)
-                continue
-            
-
-            #print(f"{username}: {msg.decode()}")
-            broadcast(msg, client_socket)
-            # broadcast(f"{username}: {msg}", client_socket)
-
-
-        except: 
-            break
-
-    with clients_lock:
-        del clients[client_socket]
-        if client_socket in client_public_keys:
-            del client_public_keys[client_socket]
-        client_socket.close()
-        print(f"{username} disconnected.")
-
 def main():
+    ### อ่านไฟล์ user_db เพื่อ เก็บ username + password ไว้ใน global variable
+    ### แล้ว ตอน login มันเช็คกับใน variable นี้ ไม่ได้เช็คจาก db ตรงๆ
+    ### ซึ่งจะเจรู้สึกว่าไม่ถูกต้องมั้ยนะ ควรต้องแก้ทีหลัง
     load_users()
     
     server_socket = socket(AF_INET, SOCK_STREAM)
@@ -151,10 +180,12 @@ def main():
                 conn_sckt.send(b"Server full. Try again later.\n")
                 conn_sckt.close()
                 continue
+            ##ส่ง public key ของเซิฟเวอร์ ไปให้ client ใช้ เพื่อเข้ารหัสข้อความ AES ในขั้นตอน login/register
             conn_sckt.send(b'SERVERPUBKEY:' + serialized_pubkey)
-            #print("New connection")
+            
 
-        # Start a new thread to handle this client's communication
+        # สร้างเทรดรันฟังค์ชั่น handle_client 
+        ## ทำไมต้องสร้างเทรด?? เพราะจะทำให้ client อื่นๆ สามารถ connect มาพร้อมกันได้ >> แยก function "handle_client()" ให้แต่ละ connection ไปเลย
         try:
             Thread(target=handle_client, args=(conn_sckt, cli_addr), daemon=True).start()
         except:
@@ -163,8 +194,11 @@ def main():
             import traceback
             traceback.print_exc()
 
+
+
+#### โค้ดจะอ่านอันนี้ก่อน ####
 if __name__ == '__main__':
     try:
-        main()
+        main() ###เริ่มต้นที่ function "main()"
     except KeyboardInterrupt:
         print("Server shutting down...")
