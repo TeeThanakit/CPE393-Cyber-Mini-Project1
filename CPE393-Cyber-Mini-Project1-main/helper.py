2b670dbe2c337dba09ca6ec8f1a3e698b8339b71
CirrusPreeminent <153276550+CirrusPreeminent@users.noreply.github.com>
2025-04-12 01:43:35 +0700

    Revert "Merge branch 'main' into Encryption&Authentication"
    This reverts commit 1f21b59b0379befb8a5931efba17acf78c58d59a, reversing
    changes made to bcd6adfe2fc68f103728037bcce5b5c11b9a0900.

--- CPE393-Cyber-Mini-Project1-main/helper.py
+++
@@ -1,5 +0,0 @@
-def fistCharToUpper(message):
-    decoded_message = message.decode('utf-8')
-    formatted_message = decoded_message[0].upper() + decoded_message[1:]
-    result = formatted_message.encode('utf-8')
-    return result
